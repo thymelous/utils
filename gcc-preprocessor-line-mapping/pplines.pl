7e69fe86bf3e76b09317ae7d66e03e155c2c2d6a
timlathy <tim.labushev@gmail.com>
2019-11-10 22:46:03 +0300

    [pp linemap] replace line numbers in compile error output

--- gcc-preprocessor-line-mapping/pplines.pl
+++ gcc-preprocessor-line-mapping/pplines.pl
@@ -3,6 +3,7 @@
 use strict;
 use IPC::Open2;
 use IPC::Open3;
+use List::MoreUtils q{natatime};
 
 my $srcpath = shift or die "Usage: $0 source\n";
 open my $src, '<', $srcpath or die "Can't open $srcpath $!\n";
@@ -18,16 +19,36 @@ close PP_IN;
 
 open3(\*CC_IN, \*CC_OUT, \*CC_ERR, 'clang -x c -') or die "Failed to start clang: $!\n";
 
+my @linemap;
+my $line = 0;
 while (<PP_OUT>) {
+  next if /^#/;
+  $line += 1;
+
   if (/(.+)#(\d+)#$/) {
+    push @linemap, $line;
+    push @linemap, $2;
     print CC_IN "$1\n";
   }
   else {
-    print CC_IN "$_\n";
+    print CC_IN "$_";
   }
 }
 close CC_IN;
 
 while (<CC_ERR>) {
-  print;
+  if (/^<stdin>:(\d+):(.+)$/) {
+    my ($line, $origline) = (1, 1);
+    my $it = natatime 2, @linemap;
+    while (my ($l, $ol) = $it->()) {
+      last if $l > $1;
+      $line = $l;
+      $origline = $ol;
+    }
+    print STDERR "$srcpath:$origline:$2\n";
+  }
+  else {
+    print STDERR;
+  }
 }
+
