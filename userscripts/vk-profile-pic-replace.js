841dbffb047a454a3ca8b9742ef4fd64d626dc07
timlathy <tim.labushev@gmail.com>
2020-03-28 14:29:37 +0300

    vk profile pic script: handle navigation between vk pages

--- userscripts/vk-profile-pic-replace.js
+++ userscripts/vk-profile-pic-replace.js
@@ -21,38 +21,38 @@
     }
   };
 
-  function replaceProfilePic(messageNode) {
-    const pics = messageNode.querySelectorAll('.im-mess-stack--photo .im_grid');
-    for (let pic of pics) {
+  let currentReplacements;
+
+  function replaceProfilePic(message) {
+    const profilePics = message.querySelectorAll('.im-mess-stack--photo .im_grid');
+    for (let pic of profilePics) {
       const user = pic.getAttribute('href').substr(1);
-      const replacement = currentProfilePicReplacements[user];
+      const replacement = currentReplacements[user];
       if (replacement)
-        pic.children[0].src = replacement;
+        pic.firstElementChild.src = replacement;
     }
   }
 
   // Replace profile pics in the currently open chat
-  const currentChat = document.querySelector('.im-page--title-main').title;
-  let currentProfilePicReplacements = perChatProfilePicReplacements[currentChat];
-  document.querySelectorAll('.im-mess-stack').forEach(replaceProfilePic);
+  const currentChat = document.querySelector('.im-page--title-main');
+  currentReplacements = currentChat && perChatProfilePicReplacements[currentChat.title];
+  currentReplacements && document.querySelectorAll('.im-mess-stack').forEach(replaceProfilePic);
 
-  // Observe switching between chats
+  // Observe page modifications
   new MutationObserver((records) => {
+    // Closing a chat and switching to a different page (news feed, communities, etc.)
+    if (!window.location.href.startsWith('https://vk.com/im?sel'))
+      return;
     for (let record of records) {
-      if (record.addedNodes.length == 1 && record.addedNodes[0].firstElementChild) {
-        const currentChat = record.addedNodes[0].firstElementChild.title;
-        currentProfilePicReplacements = perChatProfilePicReplacements[currentChat];
+      // Opening a chat
+      if (record.target.classList.contains('im-page--title-wrapper') && record.addedNodes.length == 1) {
+        const currentChat = record.addedNodes[0].firstElementChild;
+        if (currentChat)
+          currentReplacements = perChatProfilePicReplacements[currentChat.title];
       }
-    }
-  }).observe(document.querySelector('.im-page--title-wrapper'), { childList: true });
-
-  // Observe loading new messages
-  new MutationObserver((records) => {
-    if (currentProfilePicReplacements) {
-      for (let record of records) {
+      // Loading new messages
+      if (currentReplacements && record.target.classList.contains('im-page-chat-contain'))
         record.addedNodes.forEach(replaceProfilePic);
-      }
     }
-  }).observe(document.querySelector('.im-page-chat-contain'), { childList: true });
-
+  }).observe(document.getElementById('page_body'), { childList: true, subtree: true });
 })();
